a7d33a567b06b105220751b5b5178abbba0978c4
Saumy Budhiraja <saumybudhiraja@Saumys-MacBook.local>
2026-05-24 01:39:44 +0530

    Refactored logic using function

--- script.js
+++ script.js
@@ -1,14 +1,12 @@
 const result = document.getElementById("result")
 const winner = document.getElementById("winner")
-let playerChoice; 
 const choices = ["rock", "paper", "scissors"]
 let randomNumber;
 let computerChoice;
 let playerscore = 0;
 let computerscore = 0;
 
-document.getElementById("rock").addEventListener("click", function(){
-    playerChoice = 'rock'
+function playGame(playerChoice){
     randomNumber = Math.floor(Math.random()*3)
     computerChoice = choices[randomNumber]
     result.innerText = "you chose " + playerChoice + " | computer chose " + computerChoice
@@ -35,66 +33,18 @@ document.getElementById("rock").addEventListener("click", function(){
         computerscore++
         document.getElementById("computerscore").innerText = "Computer score: " + computerscore
     }
+}
+
+document.getElementById("rock").addEventListener("click", function(){
+    playGame("rock")
 })
 
 document.getElementById("paper").addEventListener("click", function(){
-    playerChoice = "paper"
-    randomNumber = Math.floor(Math.random()*3)
-    computerChoice = choices[randomNumber]
-    result.innerText = "you chose " + playerChoice + " | computer chose " + computerChoice
-
-    if(playerChoice == computerChoice){
-    winner.innerText = "Draw"}
-    else if (playerChoice == "rock" && computerChoice == "scissors"){
-        winner.innerText = "You Win"
-        playerscore++
-        document.getElementById("playerscore").innerText = "Player Score :" + playerscore
-    }
-    else if (playerChoice == "scissors" && computerChoice == "paper"){
-        winner.innerText = "You Win"
-        playerscore++
-        document.getElementById("playerscore").innerText = "Player Score :" + playerscore
-    }
-    else if (playerChoice == "paper" && computerChoice == "rock"){
-        winner.innerText = "You Win"
-        playerscore++
-        document.getElementById("playerscore").innerText = "Player Score :" + playerscore
-    }
-    else {
-        winner.innerText = "Computer Wins"
-        computerscore++
-        document.getElementById("computerscore").innerText = "Computer score: " + computerscore
-    }
+    playGame("paper")
 })
 
 document.getElementById("scissors").addEventListener("click", function(){
-    playerChoice = "scissors"
-    randomNumber = Math.floor(Math.random()*3)
-    computerChoice = choices[randomNumber]
-    result.innerText = "you chose " + playerChoice + " | computer chose " + computerChoice
-
-    if(playerChoice == computerChoice){
-    winner.innerText = "Draw"}
-     else if (playerChoice == "rock" && computerChoice == "scissors"){
-        winner.innerText = "You Win"
-        playerscore++
-        document.getElementById("playerscore").innerText = "Player Score :" + playerscore
-    }
-    else if (playerChoice == "scissors" && computerChoice == "paper"){
-        winner.innerText = "You Win"
-        playerscore++
-        document.getElementById("playerscore").innerText = "Player Score :" + playerscore
-    }
-    else if (playerChoice == "paper" && computerChoice == "rock"){
-        winner.innerText = "You Win"
-        playerscore++
-        document.getElementById("playerscore").innerText = "Player Score :" + playerscore
-    }
-    else {
-        winner.innerText = "Computer Wins"
-        computerscore++
-        document.getElementById("computerscore").innerText = "Computer score: " + computerscore
-    }
+    playGame("scissors")
 })
 
 document.getElementById("reset").addEventListener("click",function(){
